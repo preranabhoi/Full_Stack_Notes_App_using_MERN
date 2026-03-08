253914aecff411e96c576bc8a56c05205e1b72cd
Prerana Bhoi <preranabhoi32@gmail.com>
2026-03-08 21:49:37 +0530

    initialize express server with cors and basic route

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ data: "hello" });
+});
+
+app.listen(8000);
+
+module.exports = app;

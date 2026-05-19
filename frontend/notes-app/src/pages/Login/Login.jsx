572fcfdfbf03675f3d89058deb4fa7ed81cbc96d
Prerana Bhoi <preranabhoi32@gmail.com>
2026-05-19 23:23:04 +0530

    add useState hooks for form handling

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("")
+    const[error,setError]=useState(null);
+
+    const handleLogin=async(e)=>{
+        e.preventDefault()
+    }
   return (
     <>
       <Navbar />
 
       <div className="flex items-center justify-center mt-28">
         <div className="w-96 border rounded bg-white px-7 py-10">
-          <form onSubmit={() => {}}>
+          <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="text" placeholder="Email" className="input-box" />
+            <input type="text" placeholder="Email" className="input-box" 
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+            />
             
-            <PasswordInput/>
+            <PasswordInput value={password}
+            onChange={(e)=>setPassword(e.target.value)}
+            />
 
             <button type="submit" className="btn-primary">
               Login

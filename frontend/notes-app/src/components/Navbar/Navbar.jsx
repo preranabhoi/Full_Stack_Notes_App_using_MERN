39af572c102b8d7c7e6f2ad2959333d45a2cf000
Prerana Bhoi <preranabhoi32@gmail.com>
2026-05-20 19:57:28 +0530

    create responsive navbar with search and profile controls

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar=()=>{
+    const[searchQuery,setSearchQuery]=useState("")
+    const navigate=useNavigate;
+
+    const onLogout=()=>{
+        navigate("/login")
+    }
+
+    const handleSearch=()=>{
+
+    }
+
+    const onClearSearch=()=>{
+        setSearchQuery("")
+    }
     return(
         <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
             <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+        
+        <SearchBar
+        value={searchQuery}
+        onChange={({target})=>{
+            setSearchQuery(target.value)
+        }}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+        />
+
+        <ProfileInfo onLogout={onLogout}/>
         </div>
+
     )
 }
 

48a938a6425bf54bb984f0edac11f560ac9891f2
Prerana Bhoi <preranabhoi32@gmail.com>
2026-05-20 18:53:54 +0530

    integrate email validation helper and error state

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,14 +1,28 @@
 import React, { useState } from "react";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const [email,setEmail]=useState("");
-    const [password,setPassword]=useState("")
-    const[error,setError]=useState(null);
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-    const handleLogin=async(e)=>{
-        e.preventDefault()
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
     }
+
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+
+    setError("");
+
+    //Login API Call
+  };
   return (
     <>
       <Navbar />
@@ -17,14 +31,20 @@ const Login = () => {
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-            <input type="text" placeholder="Email" className="input-box" 
-            value={email}
-            onChange={(e)=>setEmail(e.target.value)}
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
-            
-            <PasswordInput value={password}
-            onChange={(e)=>setPassword(e.target.value)}
+
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
+
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
             <button type="submit" className="btn-primary">
               Login

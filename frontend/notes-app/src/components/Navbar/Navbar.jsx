91895b513d675852f7020c9b54ae0c7f28a1b4e4
Prerana Bhoi <preranabhoi32@gmail.com>
2026-05-23 22:18:08 +0530

    Add user authentication check and auto-redirect on unauthorized access

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -3,38 +3,36 @@ import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/Searchbar";
 import ProfileInfo from "../Cards/ProfileInfo";
 
-const Navbar=()=>{
-    const[searchQuery,setSearchQuery]=useState("")
-    const navigate=useNavigate;
+const Navbar = ({ userInfo }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const navigate = useNavigate();
 
-    const onLogout=()=>{
-        navigate("/login")
-    }
+  const onLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
 
-    const handleSearch=()=>{
+  const handleSearch = () => {};
 
-    }
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+  return (
+    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
+      <h2 className="text-xl font-medium text-black py-2">Notes</h2>
 
-    const onClearSearch=()=>{
-        setSearchQuery("")
-    }
-    return(
-        <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-            <h2 className="text-xl font-medium text-black py-2">Notes</h2>
-        
-        <SearchBar
+      <SearchBar
         value={searchQuery}
-        onChange={({target})=>{
-            setSearchQuery(target.value)
+        onChange={({ target }) => {
+          setSearchQuery(target.value);
         }}
         handleSearch={handleSearch}
         onClearSearch={onClearSearch}
-        />
+      />
 
-        <ProfileInfo onLogout={onLogout}/>
-        </div>
-
-    )
-}
+      <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
+    </div>
+  );
+};
 
 export default Navbar;
